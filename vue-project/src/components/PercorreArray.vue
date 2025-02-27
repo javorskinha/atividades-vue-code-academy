7e6ac565401e9b403a4cb3deca2666e82f5a1e89
Javorskinha <andrielijavorski@gmail.com>
2025-02-27 16:56:50 -0300

    getLista

--- vue-project/src/components/PercorreArray.vue
+++ vue-project/src/components/PercorreArray.vue
@@ -1,1 +1,29 @@
-<template></template>
+<template>
+    <div>
+        <ul>
+            <li>{{  }}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+    const id = ref('')
+
+    const nome = ref('')
+
+    async function getLista (){
+        const req = await fetch('http://localhost:3000/lista')
+
+        const data = await req.json()
+
+        id = data.id
+        nome = data.nome
+    }
+
+</script>
+
+
+
+
+
