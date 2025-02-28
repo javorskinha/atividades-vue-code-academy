688eaac610ec37d38475b584156a5f612b5d8b42
Javorskinha <andrielijavorski@gmail.com>
2025-02-28 14:46:19 -0300

    fim dessas atividades

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,25 +1,35 @@
 <script setup>
+import { ref } from 'vue';
 import VmodelData from './components/VmodelData.vue'
 import PropsPersonalizada from './components/PropsPersonalizada.vue'
 import CompContador from './components/CompContador.vue'
 import MostraFotos from './components/MostraFotos.vue'
 import MudaFoto from './components/MudaFoto.vue'
+import PercorreArray from './components/PercorreArray.vue'
 
 function recebeEvento(mensagem){
   window.alert(mensagem)
 }
+
+    const corInicial = ref('')
+
 </script>
 
 <template>
-  <h1>Olá</h1>
-  <VmodelData/>
-  <PropsPersonalizada nome="Rafael"/>
-  <hr>
-  <CompContador @notificacao="recebeEvento"/>
-  <hr>
-  <MostraFotos/>
-  <hr>
-  <MudaFoto />
+  <div :style="{backgroundColor: corInicial}">
+    <h1>Olá</h1>
+    <VmodelData/>
+    <PropsPersonalizada nome="Rafael"/>
+    <hr>
+    <CompContador @notificacao="recebeEvento"/>
+    <hr>
+    <MostraFotos/>
+    <hr>
+    <MudaFoto />
+    <PercorreArray />
+    <hr>
+    <input type="color" v-model="corInicial">
+  </div>
 </template>
 
 <style>
--- vue-project/src/components/PercorreArray.vue
+++ vue-project/src/components/PercorreArray.vue
@@ -1,24 +1,33 @@
 <template>
     <div>
-        <ul>
-            <li>{{  }}</li>
+        <h1>Lista de Mercado:</h1>
+        <button @click="exibe()">exibir lista</button>
+        <ul type="square" v-if="mostrar">
+            <li v-for="item in lista" :key="item.id">{{ item.nome }}</li>
         </ul>
     </div>
 </template>
 
-<script>
+<script setup>
 import { ref } from 'vue'
-    const id = ref('')
 
-    const nome = ref('')
+    const lista = ref([
+        {id: 1, nome: "pão"},
+        {id: 2, nome: "leite"},
+        {id: 3, nome: "ovos"},
+        {id: 4, nome: "trigo"},
+        {id: 5, nome: "arroz"},
+        {id: 6, nome: "feijão"},
+        {id: 7, nome: "alface"},
+        {id: 8, nome: "brócolis"},
+        {id: 9, nome: "batata"},
+        {id: 10, nome: "carne"}
+    ])
 
-    async function getLista (){
-        const req = await fetch('http://localhost:3000/lista')
+    const mostrar = ref(false)
 
-        const data = await req.json()
-
-        id = data.id
-        nome = data.nome
+    function exibe(){
+        mostrar.value = !mostrar.value
     }
 
 </script>
